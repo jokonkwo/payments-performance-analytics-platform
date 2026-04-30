421f650427a78410e7be2e462ae6ccd81117c3b9
jokonkwo <62678691+jokonkwo@users.noreply.github.com>
2026-04-30 03:48:42 -0700

    Update fct_payment_transactions.sql

--- models/marts/fct_payment_transactions.sql
+++ models/marts/fct_payment_transactions.sql
@@ -1,17 +1,20 @@
--- Core fact table for payment transaction analytics
+-- Core payment transaction fact table
 
 SELECT
     payment_creation_day,
     txn_reference,
+
     bin,
-    funding_source,
     issuer_country_code,
+    funding_source,
     shopper_interaction,
     mcc,
 
     transaction_amount,
     interchange_amount,
+    interchange_rate,
 
-    interchange_amount / NULLIF(transaction_amount, 0) AS interchange_rate
+    interchange_cost_tier,
+    auth_quality_segment
 
-FROM stg_payment_transactions
+FROM int_payment_enriched
